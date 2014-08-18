9ed422058daf11449ca1d4f00698ff4a12751a28
roselmamendes <roselma.mendes@gmail.com>
2014-08-18 18:48:58 -0300

    Alteracoes do dia 18/08/14

---
+++ OOP/Entertaiment_Center.py
@@ -0,0 +1,15 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                    "A story of a boy and his toys that come to life",
+                        "http://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+                        "www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                    "A marine on an alien planet",
+                        "http://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+                        "www.youtube.com/watch?v=xTWLBuTak6I")
+
+print(avatar.storyline)
---
+++ OOP/media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self,movie_title,movie_storyline,movie_poster_image_url,movie_trailer_youtube_url):
+        self.title
+        self.storyline
+        self.poster_image_url
+        self.trailer_youtube_url

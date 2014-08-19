52b6140270f38c78079def967bb26b10c4bc9023
roselmamendes <roselma.mendes@gmail.com>
2014-08-19 16:14:27 -0300

    Mudanças do dia 19/08/14

--- OOP/Entertaiment_Center.py
+++ OOP/Entertaiment_Center.py
@@ -3,13 +3,13 @@ import media
 toy_story = media.Movie("Toy Story",
                     "A story of a boy and his toys that come to life",
                         "http://en.wikipedia.org/wiki/File:Toy_Story.jpg",
-                        "www.youtube.com/watch?v=KYz2wyBy3kc")
+                        "http://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
 
-avatar = media.Movie("Avatar",
-                    "A marine on an alien planet",
-                        "http://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
-                        "www.youtube.com/watch?v=xTWLBuTak6I")
+avatar = media.Movie("Avatar","A marine on an alien planet",
+                     "http://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+                     "http://www.youtube.com/watch?v=xTWLBuTak6I")
 
-print(avatar.storyline)
+#print(avatar.storyline)
+avatar.show_trailer()
--- OOP/media.py
+++ OOP/media.py
@@ -1,6 +1,12 @@
+import webbrowser
+
 class Movie():
-    def __init__(self,movie_title,movie_storyline,movie_poster_image_url,movie_trailer_youtube_url):
-        self.title
-        self.storyline
-        self.poster_image_url
-        self.trailer_youtube_url
+    def __init__(self,movie_title,movie_storyline,movie_poster_image_url,
+                 movie_trailer_youtube_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_image_url
+        self.trailer_youtube_url = movie_trailer_youtube_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
